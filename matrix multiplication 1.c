1a448afd4930e4c988909b99d71e28ce61591e25
sanjai7779 <113322953+sanjai7779@users.noreply.github.com>
2022-10-06 11:28:19 +0530

    Create matrix multiplication 1.c

---
+++ matrix multiplication 1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+ int a[2][2],b[2][2],c[2][2],i,j;
+ printf("Enter the elements in the  1 st array:");
+ for(i=0;i<2;i++){
+ for(j=0;j<2;j++){
+ scanf("%d",&a[i][j]);
+ }
+ }
+ printf("Enter the elements in the 2 nd array:");
+ for(i=0;i<2;i++){
+  for(j=0;j<2;j++){
+    scanf("%d",&b[i][j]);
+  }
+  }
+  printf("matrix addition:");
+ for(i=0;i<2;i++){
+  for(j=0;j<2;j++){
+  c[i][j]=a[i][j]*b[i][j];
+    printf("%d",c[i][j]);
+    printf("\n");
+  }
+  }
+  return 0;
+}
